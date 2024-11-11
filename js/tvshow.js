304f5503913068eff3be634912f154263512e715
CharlotteJoyce <charlottejdcb@gmail.com>
2024-11-11 20:55:20 +0800

    Fixed card carousel using js swiper

---
+++ js/tvshow.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      loop: true,
+    });
+  });
