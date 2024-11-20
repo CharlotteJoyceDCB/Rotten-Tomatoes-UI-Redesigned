b6fe6c4cffee80be97bd7a7ce5a8f590dd3eeb3e
CharlotteJoyce <charlottejdcb@gmail.com>
2024-11-20 17:08:48 +0800

    Added a scroll to top floating button on tv shows page

--- js/tvshow.js
+++ js/tvshow.js
@@ -13,3 +13,20 @@ document.addEventListener('DOMContentLoaded', function () {
       loop: true,
     });
   });
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+scrollToTopBtn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
